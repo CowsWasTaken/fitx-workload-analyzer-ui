c5a04dc35450fe2128c2c73bdeeb9c326454a832
Felix J <felix.hannes.fj@gmail.com>
2022-08-07 00:12:26 +0200

    fix: weekdays in correct order

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ChartConfiguration, ChartOptions} from "chart.js";
+import {ChartConfiguration, ChartOptions, ScriptableLineSegmentContext, TooltipItem} from "chart.js";
 import {WorkloadRecord} from "../model/workloadRecord";
 import {ActivatedRoute} from "@angular/router";
 import 'chartjs-adapter-moment';
@@ -15,13 +15,13 @@ export class ChartComponent implements OnInit {
   studioInfo = 'Fitx - Fürth Hardhöhe'
 
   colorStore = new Map<string, string>([
-    ['Monday','rgba(51,194,46,0.2)'],
-    ['Tuesday','rgba(131,46,143,0.2)'],
-    ['Wednesday','rgba(197,52,101,0.2)'],
-    ['Thursday','rgba(168,127,58,0.2)'],
-    ['Friday','rgba(42,88,140,0.2)'],
-    ['Saturday','rgba(66,199,183,0.2)'],
-    ['Sunday','rgba(88,56,155,0.2)'],
+    ['Monday', 'rgba(51,194,46,0.2)'],
+    ['Tuesday', 'rgba(131,46,143,0.2)'],
+    ['Wednesday', 'rgba(197,52,101,0.2)'],
+    ['Thursday', 'rgba(168,127,58,0.2)'],
+    ['Friday', 'rgba(42,88,140,0.2)'],
+    ['Saturday', 'rgba(66,199,183,0.2)'],
+    ['Sunday', 'rgba(88,56,155,0.2)'],
   ])
 
   data: WorkloadRecord[] = []
@@ -59,8 +59,8 @@ export class ChartComponent implements OnInit {
           // borderColor: 'black',
           backgroundColor: 'rgba(255,0,0,0.3)',
           segment: {
-            borderColor: (ctx: any) => this.colorize(ctx),
-            backgroundColor: (ctx: any) => this.colorize(ctx),
+            borderColor: (ctx: ScriptableLineSegmentContext) => this.colorize(ctx),
+            backgroundColor: (ctx: ScriptableLineSegmentContext) => this.colorize(ctx),
           }
         }
       ],
@@ -73,6 +73,10 @@ export class ChartComponent implements OnInit {
         title: {
           text: this.studioInfo,
           display: true
+        }, tooltip: {
+          callbacks: {
+            footer: (tooltipItems: TooltipItem<'line'>[]): string | string[] => this.createFooter(tooltipItems)
+          }
         }
       },
       elements: {
@@ -109,7 +113,7 @@ export class ChartComponent implements OnInit {
     };
   }
 
-  colorize(ctx: any) {
+  colorize(ctx: ScriptableLineSegmentContext) {
     const index = ctx.p0DataIndex
     return this.getColorForTimestamp(this.data[index].timestamp)
   }
@@ -124,11 +128,19 @@ export class ChartComponent implements OnInit {
   }
 
   getColorForTimestamp(timestamp: number) {
-    const dayOfWeekName = new Date(timestamp * 1000).toLocaleDateString(
-      'en-US', {weekday: 'long'}
-    );
+    const dayOfWeekName = this.timestampToWeekday(timestamp)
     return this.colorStore.get(dayOfWeekName)
   }
 
+  timestampToWeekday(timestamp: number) {
+    return new Date(timestamp * 1000).toLocaleDateString(
+      'en-US', {weekday: 'long'}
+    );
+  }
+
+  createFooter(tooltipItems: TooltipItem<'line'>[]): string | string[] {
+    const timestamp = this.data[tooltipItems[0].dataIndex].timestamp
+    return this.timestampToWeekday(timestamp)
+  }
 
 }
